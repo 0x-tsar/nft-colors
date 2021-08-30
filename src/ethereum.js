2173f6871309a1826cca5d8494ecad4420027ec0
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-08-30 06:25:17 -0300

    styling components

--- src/ethereum.js
+++ src/ethereum.js
@@ -6,6 +6,7 @@ const getBlockchain = () =>
     window.addEventListener("load", async () => {
       if (window.ethereum) {
         await window.ethereum.enable();
+
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
 
